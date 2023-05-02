3ec34802a3abc5c5cb3e654722aa882b78bac625
Joanna Pathenay <j.pathenay@gmail.com>
2023-05-02 15:26:25 +0200

    A1-ajout header et footer

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/bootstrap.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>PâteFinder</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>PâteFinder</h1>
+            <p>Gérez vos fiches de personnages pour vos parties de Jeu de Rôle !</p>
+        </div>
+    </header>
+
