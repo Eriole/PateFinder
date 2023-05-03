82d50e3961a1301985ec05f427a6fce5ece38e5f
hichem <hichem.meneceur@outlook.fr>
2023-05-03 10:57:23 +0200

    F3 - Correction NaN JS

--- script/main.js
+++ script/main.js
@@ -1,23 +1,23 @@
 let stats = document.querySelectorAll("[data-type]");
-let count = document.querySelector('#count');
+let count = document.querySelector("#count");
 
-stats.forEach((stat)=> {
-    //Event Listener onchange and on key up
-    stat.addEventListener("change", addition);
-    stat.addEventListener("keyup", addition);
+stats.forEach((stat) => {
+  //Event Listener onchange and on key up
+  stat.addEventListener("change", addition);
+  stat.addEventListener("keyup", addition);
 });
 
 // Add values to a variable sum
-function addition () {
-    let stats = document.querySelectorAll("[data-type]");
-    let sum = 0;
-    stats.forEach( (stat) => {
-        sum += parseInt(stat.value);
-    });
-    displaySum(sum);
+function addition() {
+  let stats = document.querySelectorAll("[data-type]");
+  let sum = 0;
+  stats.forEach((stat) => {
+    sum += parseInt(stat.value || 0);
+  });
+  displaySum(sum);
 }
 
 //Display sentence with substraction
-function displaySum (sum){
-    count.innerHTML = `Reste entre ${60 - sum} et ${80-sum} pts à distribuer.`;
+function displaySum(sum) {
+  count.innerHTML = `Reste entre ${60 - sum} et ${80 - sum} pts à distribuer.`;
 }
