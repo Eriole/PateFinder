042487aa85bcf5f4cb899a825641eab7c5a95b03
Joanna Pathenay <j.pathenay@gmail.com>
2023-05-10 12:24:50 +0200

    debut html

--- pages/character_sheet.php
+++ pages/character_sheet.php
@@ -1,1 +1,69 @@
 <?php
+
+?>
+<div class="container d-flex flex-column align-items-center">
+    <div class="d-flex justify-content-around align-items-center gap-3 w-75">
+        <h2>Nom personnage</h2>
+        <i class="fa-solid fa-pen-to-square"></i>
+    </div>
+
+    <hr class="w-50">
+
+    <!-- Statistics -->
+    <section class="w-50">
+        <div class="d-flex justify-content-around align-items-center gap-3">
+            <h3>Caractéristiques</h3>
+            <i class="fa-solid fa-pen-to-square"></i>
+        </div>
+
+        <div class="bg-gray p-2 rounded">
+            <p><span class="p-2 dark-gray rounded me-3">12</span> Initiative (INIT) </p>
+
+            <div class="d-flex justify-content-between align-items-center">
+                <ul class="list-unstyled my-0">
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> Points de vie max </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> Points de vie actuels </p>
+                    </li>
+                </ul>
+                <ul class="list-unstyled">
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> Points de magie max </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> Points de magie actuels </p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="d-flex justify-content-around mx-auto align-items-center w-50 bg-light-gray p-3 rounded ">
+                <ul class="list-unstyled my-0">
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> FOR </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> DEX </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> CONST </p>
+                    </li>
+                </ul>
+                <ul class="list-unstyled my-0">
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> INT </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> SAG </p>
+                    </li>
+                    <li>
+                        <p><span class="p-2 dark-gray rounded me-3">12</span> CHA </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Skill and Stuff -->
+</div>
