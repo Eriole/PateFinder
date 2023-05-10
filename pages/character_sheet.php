2c15629d44ed325628e5677cdcf363b73721ad32
Joanna Pathenay <j.pathenay@gmail.com>
2023-05-10 13:42:52 +0200

    F5- structure html

--- pages/character_sheet.php
+++ pages/character_sheet.php
@@ -1,23 +1,25 @@
 <?php
 
 ?>
-<div class="container d-flex flex-column align-items-center">
-    <div class="d-flex justify-content-around align-items-center gap-3 w-75">
-        <h2>Nom personnage</h2>
-        <i class="fa-solid fa-pen-to-square"></i>
+<div class="container d-flex flex-column align-items-center my-5 gap-5">
+    <div class="w-75">
+        <div class="d-flex justify-content-around align-items-center">
+            <h2>Nom personnage</h2>
+            <i class="fa-solid fa-pen-to-square"></i>
+        </div>
+        <hr class="mx-auto w-75">
     </div>
-
-    <hr class="w-50">
 
     <!-- Statistics -->
     <section class="w-50">
+        <!-- Title -->
         <div class="d-flex justify-content-around align-items-center gap-3">
             <h3>Caractéristiques</h3>
             <i class="fa-solid fa-pen-to-square"></i>
         </div>
-
-        <div class="bg-gray p-2 rounded">
-            <p><span class="p-2 dark-gray rounded me-3">12</span> Initiative (INIT) </p>
+        <!-- Display -->
+        <div class="bg-gray px-3 py-5 rounded">
+            <p class="mb-4"><span class="p-2 dark-gray rounded me-3">12</span> Initiative (INIT) </p>
 
             <div class="d-flex justify-content-between align-items-center">
                 <ul class="list-unstyled my-0">
@@ -66,4 +68,62 @@
     </section>
 
     <!-- Skill and Stuff -->
+    <section class="w-75 d-flex justify-content-around gap-3">
+        <!-- Skill -->
+        <article class="w-50">
+            <h3 class="text-center">Compétences</h3>
+            <div class="bg-gray px-2 py-4 rounded">
+                <p class="text-end"><i class="fa-solid fa-plus"></i></p>
+                <!-- Display -->
+                <table class="table table-striped">
+                    <tr>
+                        <th>Nom</th>
+                        <th class="text-center">Caractéristique</th>
+                        <th class="text-center">Niveau (0-5)</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>Voleur à la sauvette</td>
+                        <td class="text-center">DEX</td>
+                        <td class="text-center">2</td>
+                        <td class="text-end"><i class="fa-solid fa-pen-to-square"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Voleur à la sauvette</td>
+                        <td class="text-center">DEX</td>
+                        <td class="text-center">2</td>
+                        <td class="text-end"><i class="fa-solid fa-pen-to-square"></i></td>
+                    </tr>
+                </table>
+            </div>
+        </article>
+        <!-- Stuff -->
+        <article class="w-50">
+            <h3 class="text-center">Equipement</h3>
+            <div class="bg-gray px-2 py-4 rounded">
+                <p class="text-end"><i class="fa-solid fa-plus"></i></p>
+                <!-- Display -->
+                <table class="table table-striped">
+                    <tr>
+                        <th>Nom</th>
+                        <th>Dégâts</th>
+                        <th>Niveau (0-5)</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>Sandales destructrices</td>
+                        <td class="text-center">18</td>
+                        <td class="text-center">2</td>
+                        <td class="text-end"><i class="fa-solid fa-pen-to-square"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Voleur à la sauvette</td>
+                        <td class="text-center">18</td>
+                        <td class="text-center">2</td>
+                        <td class="text-end"><i class="fa-solid fa-pen-to-square"></i></td>
+                    </tr>
+                </table>
+            </div>
+        </article>
+    </section>
 </div>
